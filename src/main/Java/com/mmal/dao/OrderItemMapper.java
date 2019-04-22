b0c83d574ace0f4087e627d60f903e7bbb0456bc
yanlanliu2018 <763468871@qq.com>
2019-04-22 18:32:21 +0800

    project init commit

---
+++ src/main/Java/com/mmal/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.mmal.dao;
+
+import com.mmal.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
